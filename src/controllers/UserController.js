e6689b5743337ec16a2ded3ba984bfb2c1984502
Rubens Victor <rubensvictorsantos@gmail.com>
2022-02-22 16:35:57 -0300

    criada rota para fazer a paginação user e criação middleware para a paginação do usuário 22/02/2022

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -7,35 +7,43 @@ const connection = require("../database");
 User.init(connection);
 
 module.exports = {
+    async users(req, res) {
+        const users = await User.findAll();
+        return res.json(users);
+    },
 
     async login(req, res) {
-        const { email, senha } = req.body;
+        try {
+            const { email, senha } = req.body;
+            const user = await User.findOne({ where: { email } });
+            const userStringify = JSON.stringify(user);
+            const userJSON = JSON.parse(userStringify);
 
-        const user = await User.findOne({ where: { email } });
-
-        const userStringify = JSON.stringify(user);
-
-        const userJSON = JSON.parse(userStringify);
-
-        if (userJSON) {
-
-            if (brcypt.compareSync(senha, userJSON.senha)) {
-
-                const id = userJSON.cod_usuario;
-
-                // auth ok
-                const token = jwt.sign({ id }, process.env.SECRET, {
-                    expiresIn: 300 // expires in 5min
-                });
-
-                return res.json({ user: user, auth: true, token: token });
-
-            } else {
-                return res.json({ error: "Usuário e/ou senha inválido(s)" });
-
+            if (!userJSON) {
+                res.status(404);
+                throw new Error("Usuário não encontrado");
             }
-        } else {
-            return res.json({ error: "Usuário não existe" });
+
+            if (!brcypt.compareSync(senha, userJSON.senha)){ 
+                res.status(401);
+                throw new Error("Usuário e/ou senha inválido(s)");
+            }
+
+            const id = userJSON.cod_usuario;
+
+            // auth ok
+            const token = jwt.sign({ id }, process.env.SECRET, {
+                expiresIn: 300 // expires in 5min
+            });
+
+            return res.json({ user: user, auth: true, token: token });
+
+        } catch (error) {
+
+            return res.json({
+                error: 'Error',
+                message: error.message
+            });
         }
     },
 
@@ -44,41 +52,67 @@ module.exports = {
     },
 
     /**  */
-    async selectById(req, res) {
-        const id = req.params.id;
+    async userById(req, res) {
+        const { id } = req.params;
         const user = await User.findByPk(id);
-
         return res.json(user);
     },
 
-    async selectAll(req, res) {
+    async createUser(req, res) {
 
-        let allUser = await User.findAll({order: [['nome', 'DESC']]});
+        try {
+            const { nome, email, senha, cod_nivel } = req.body;
 
-        return res.json(allUser);
-    },
+            const emailJaCadastrado = await User.findOne({ where: { email } });
 
-    async insert(req, res) {
-        const { nome, email, senha, cod_nivel } = req.body;
+            if (emailJaCadastrado == null) {
 
-        const emailJaCadastrado = await User.findOne({ where: { email } });
+                const salt = brcypt.genSaltSync(10);
 
-        if (emailJaCadastrado == null) {
+                const senhaCripto = brcypt.hashSync(senha, salt);
 
-            const salt = brcypt.genSaltSync(10);
+                const usuarioCriado = await User.create({
+                    nome, email, senha: senhaCripto, id_nivel: cod_nivel
+                });
 
-            const senhaCripto = brcypt.hashSync(senha, salt);
+                return res.json(usuarioCriado);
 
-            const usuarioCriado = await User.create({
-                nome, email, senha: senhaCripto, id_nivel: cod_nivel
+            } else {
+                return res.json({ status: "email já cadastrado" });
+            }
+        } catch (error) {
+            return res.status(400).send({
+                message:
+                    error.message
             });
-
-            return res.json(usuarioCriado);
-
-        } else {
-            return res.json({ status: "email já cadastrado" });
         }
 
     },
-      
+
+    async usersPagination(req, res) {
+
+        try {
+
+            const { offset, limit } = req.params;
+
+            if (isNaN(offset) || isNaN(limit)) throw new Error("Parametro inválido!");
+
+            if ((parseInt(limit) < 0) || (parseInt(offset) < 0)) throw new Error("Parametro 'limit' não pode ser menor que zero!");
+
+            const products = await User.findAndCountAll({
+                offset: parseInt(offset),
+                limit: parseInt(limit)
+            })
+
+            return res.json(products);
+        } catch (error) {
+            return res.status(500).json({
+                error: {
+                    code: '500',
+                    message: error.message
+                },
+                payload: null
+            });
+        }
+    },
 }
